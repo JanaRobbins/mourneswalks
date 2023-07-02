d5085b0134e2794467b8f0cae3372071e882e153
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-07-02 22:54:54 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -10,10 +10,10 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   
   // Output the result in an element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "  days to start -  81st July 2023 ";
+  document.getElementById("countdown").innerHTML = days + "  days to start -  1st July 2023 ";
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Walk started on 8th of July 2023, email us or check our web page for another walk in the future";
+    document.getElementById("demo").innerHTML = "Walk started on 1st of July 2023, email us or check our web page for another walk in the future";
   }
 }, 1000);
