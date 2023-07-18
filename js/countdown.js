184d1b9200396b7d9c2e2847f9058d5fa1177725
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2023-07-18 22:43:59 +0100

    Update countdown.js
    changing dates

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
-// Set the date we're counting down to - 8. 7. 2023
-var countDownDate = new Date("Oct 8, 2023").getTime();
+// Set the date we're counting down to - 1. 10. 2023
+var countDownDate = new Date("Oct 1, 2023").getTime();
 // Update the count down every day
 var x = setInterval(function() {
   // Get today's date 
@@ -10,10 +10,10 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   
   // Output the result in an element with id="countdown"
-  document.getElementById("countdown").innerHTML = days + "  days to start -  1st July 2023 ";
+  document.getElementById("countdown").innerHTML = days + "  days to start -  1st October 2023 ";
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Walk started on 1st of July 2023, email us or check our web page for another walk in the future";
+    document.getElementById("demo").innerHTML = "Walk started on 1st of Octobr 2023, email us or check our web page for another walk in the future";
   }
 }, 1000);
