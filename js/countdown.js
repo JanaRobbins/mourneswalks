f956496c016186a9c3243d8d6ba8bbe3e931aa41
JanaRobbins <128938375+JanaRobbins@users.noreply.github.com>
2024-06-06 15:12:02 +0100

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,4 +1,4 @@
-// Set the date we're counting down to - 21. 1. 2024
+// Set the date we're counting down to - 21. 1. 2025
 var countDownDate = new Date("Jan 21, 2025").getTime();
 // Update the count down every day
 var x = setInterval(function() {
@@ -14,6 +14,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Walk started on 21st of January 2024, email us or check our web page for another walk in the future";
+    document.getElementById("demo").innerHTML = "Walk started on 21st of January 2025, email us or check our web page for another walk in the future";
   }
 }, 1000);
